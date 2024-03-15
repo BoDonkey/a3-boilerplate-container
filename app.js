99986feda432e4519facaa4b6560671dc4bc84cb
Robert Means <robert@robertmeans.net>
2024-03-15 14:07:33 -0400

    Complicated changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('apostrophe')({
-  shortName: 'a3-boilerplate',
+  shortName: 'kosarin-a3-boilerplate',
   modules: {
     // Apostrophe module configuration
     // *******************************
@@ -29,6 +29,8 @@ require('apostrophe')({
     // `asset` supports the project's webpack build for client-side assets.
     asset: {},
     // The project's first custom page type.
-    'default-page': {}
+    'default-page': {},
+    '@apostrophecms/event': {},
+    '@apostrophecms/event-page': {}
   }
 });
---
+++ modules/@apostrophecms/event/index.js
@@ -0,0 +1,62 @@
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+
+module.exports = {
+  init(self) {
+    self.apos.template.addFilter({
+      localizeDate: self.localizeDate
+    });
+  },
+  handlers(self) {
+    return {
+      '@apostrophecms/page:beforeSend': {
+        webpack(req) {
+          req.data.timezone = process.env.TIMEZONE || 'America/New_York';
+        }
+      }
+    };
+  },
+  methods(self) {
+    return {
+      denormalizeDatesAndTimes(piece) {
+        dayjs.extend(utc);
+        // Parse our dates and times
+        let startTime = piece.startTime;
+        const startDate = piece.startDate;
+        let endTime = piece.endTime;
+        let endDate;
+
+        if (piece.dateType === 'consecutive') {
+          endDate = piece.endDate;
+        } else {
+          piece.endDate = piece.startDate;
+          endDate = piece.startDate;
+        }
+
+        if (piece.allDay) {
+          startTime = '00:00:00';
+          endTime = '23:59:59';
+        }
+
+        if (piece.dateType === 'repeat') {
+          piece.hasClones = true;
+        }
+
+        piece.start = new Date(startDate + 'T' + startTime + 'Z');
+        piece.end = new Date(endDate + 'T' + endTime + 'Z');
+      },
+      localizeDate(date, format, allDay = false, zone = 'America/New_York') {
+        dayjs.extend(utc);
+        dayjs.extend(timezone);
+        if (!date) {
+          return '';
+        }
+        const baseDate = dayjs(date);
+        const utcDate = baseDate.utc().format(format);
+        const localizedDate = baseDate.tz(zone).format(format);
+        return allDay ? utcDate : localizedDate;
+      }
+    };
+  }
+};
